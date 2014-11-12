1b1e0ade41d199e5a087b5849ce44beca7af6da4
Scott Burton <scotthburton@gmail.com>
2014-11-12 09:30:49 -0700

    Added a print ln

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -16,6 +16,8 @@ public class GitTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello");
     }
     
 }
